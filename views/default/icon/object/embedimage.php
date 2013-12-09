b7acaf8df62b0017dc1230b9382f6d31822fdc33
Jeff Tilson <jrtilson@gmail.com>
2013-12-09 16:28:58 -0500

    Linking to the master url for embed image full view

--- views/default/icon/object/embedimage.php
+++ views/default/icon/object/embedimage.php
@@ -9,6 +9,49 @@
  * @link http://www.thinkglobalschool.com/
  * 
  */
-// Just user the file icon view
-echo elgg_view('icon/object/file', $vars);
-?>
+$entity = $vars['entity'];
+
+$sizes = array('small', 'medium', 'large', 'tiny', 'master', 'topbar');
+// Get size
+if (!in_array($vars['size'], $sizes)) {
+	$vars['size'] = "medium";
+}
+
+$title = $entity->title;
+$title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8', false);
+
+$url = $entity->getURL();
+if (isset($vars['href'])) {
+	$url = $vars['href'];
+}
+
+$class = '';
+if (isset($vars['img_class'])) {
+	$class = $vars['img_class'];
+}
+if ($entity->thumbnail) {
+	$class = "class=\"elgg-photo $class\"";
+} else if ($class) {
+	$class = "class=\"$class\"";
+}
+
+$img_src = $entity->getIconURL($vars['size']);
+$img_src = elgg_format_url($img_src);
+
+$master_src = $entity->getIconURL('master');
+
+$img = "<img $class src=\"$img_src\" alt=\"$title\" />";
+
+if ($master_src) {
+	$params = array(
+		'href' => $master_src,
+		'text' => $img,
+		'is_trusted' => true,
+	);
+	if (isset($vars['link_class'])) {
+		$params['class'] = $vars['link_class'];
+	}
+	echo elgg_view('output/url', $params);
+} else {
+	echo $img;
+}
